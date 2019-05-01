d70acdf99d80eafa331dc4081859d674164893bc
Matheus Alves <theuves@gmail.com>
2019-04-30 21:50:11 -0400

    Corrige e melhora algumas coisas

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ window.addEventListener('load', () => {
   const ns = new NoSleep()
   const btn = document.querySelector('#button')
 
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('is-active')
+  function enable() {
+    ns.enable()
+    btn.innerText = 'Desativar NoSleep.js'
+  }
+  function disable() {
+    ns.disable()
+    btn.innerText = 'Ativar NoSleep.js'
+  }
 
-    btn.classList.contains('is-active')
-      ? ns.disable()
-      : ns.enable()
+  btn.addEventListener('click', () => {
+    btn.classList.contains('is-enable') ? enable() : disable()
+    btn.classList.toggle('is-enable')
   })
 })
