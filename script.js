916b388f2d225c9a5ede48768244b22b67c74e71
Matheus Alves <theuves@gmail.com>
2019-04-30 22:07:56 -0400

    Corrige coisas

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ window.addEventListener('load', () => {
   const ns = new NoSleep()
   const btn = document.querySelector('#button')
 
-  function enable() {
-    ns.enable()
-    btn.innerText = 'Ativar NoSleep.js'
-  }
-  function disable() {
-    ns.disable()
-    btn.innerText = 'Desativar NoSleep.js'
-  }
-
   btn.addEventListener('click', () => {
-    btn.classList.contains('is-enable') ? disable() : enable()
-    btn.classList.toggle('is-enable')
+    const isEnable = btn.classList.contains('is-enable')
+
+    if (isEnable) {
+      ns.disable()
+      btn.innerText = 'Ativar NoSleep.js'
+      btn.classList.remove('is-enable')
+    } else {
+      ns.enable()
+      btn.innerText = 'Desativar NoSleep.js'
+      btn.classList.add('is-enable')
+    }
   })
 })
